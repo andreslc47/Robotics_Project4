09f89bca769c873e7a8d4624a4cf839342e55c0c
andreslc47 <andreslc47@hotmail.com>
2021-09-10 15:11:02 -0500

    My last version of the Project

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/andres/Robotics_Project4/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/andres/Robotics_Project4/devel;/home/andres/turtlebot_ws/devel;/opt/ros/melodic'.split(';') if '/home/andres/Robotics_Project4/devel;/home/andres/turtlebot_ws/devel;/opt/ros/melodic' != '' else []
